d47ed699fc354089d29c7179e78eeff946cbdb1d
Cosifran <franciscobackeend@gmail.com>
2024-07-11 21:57:00 -0500

    Logo added to header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import {Roboto} from "next/font/google";
 import "./globals.css";
-import Favicon from "../../public/favicon.ico";
+import Favicon from "../../public/img/favicon.ico";
 //Import own components
 import CursorAnimate from "@/components/CursorAnimate/CursorAnimate";
 
@@ -15,11 +15,13 @@ export const metadata = {
     "SOMOS STUDIO 9.13, UN GRUPO DE CREATIVOS ENFOCADO EN CONECTARTALENTO CON MARCAS Y EMPRESAS APORTANDO NUESTRA VISIÓN E IDEAS.",
   icons: [{rel: "icon", url: Favicon.src}],
 };
-<link rel="icon" href="../../public/favicon.ico" sizes="any" />;
 
 export default function RootLayout({children}) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/img/favicon.ico" sizes="any" />
+      </head>
       <body className={roboto.className}>
         <CursorAnimate />
         {children}
