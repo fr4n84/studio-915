f2b4267289ea78226aeb5bee4dec120e7966b6a7
Francisco Estrada <franciscobackend@gmail.com>
2024-08-03 09:44:45 -0500

    (FIX) Spelling and grammar will be corrected on the site.

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -10,9 +10,9 @@ export default function contacts() {
       
       <Nav />
       <Grid className="md:px-5 md:pe-7 contact-container" container spacing={2}>
-        <Grid item xs={12} lg={12}>
-          <h2 className="px-4 mt-7 text-center mb-5 md:mt-0 md:mb-0 uppercase font-bold studio_text-big  md:px-0">
-            work with us &quot;
+        <Grid xs={12} lg={12} className="ps-1">
+          <h2 className=" mt-7 text-center mb-5 md:mt-0 md:mb-0 uppercase font-bold studio_text-big md:px-0">
+            work with us"
           </h2>
         </Grid>
       </Grid>
